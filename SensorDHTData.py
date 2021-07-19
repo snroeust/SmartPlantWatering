bc5e143cb31134a569aa545de6b9dcdaa7c9db82
root <root@DESKTOP-UAFRR4C.localdomain>
2021-07-20 01:05:31 +0200

    Fix

--- SensorDHTData.py
+++ SensorDHTData.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 # read data using pin 14
-instance = dht11.DHT11(pin = 14)
+instance = dht11.DHT11(pin = 16)
 result = instance.read()
 
 if result.is_valid():
