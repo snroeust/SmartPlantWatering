1f1cd3281d23e825b80d057d91e837f0fc5ac9a5
root <root@DESKTOP-UAFRR4C.localdomain>
2021-07-21 01:03:57 +0200

    fix

--- sensor_interface.cpp
+++ sensor_interface.cpp
@@ -100,7 +100,29 @@ int SensorInterface::readSerial()
    char result[4];
    int index = 0;
 
-   while (serialDataAvail(fd))
+   constexpr int UART_INPUT_MAX_SIZE = 1024;
+   char uartInput[UART_INPUT_MAX_SIZE+1];
+
+   while(serialDataAvail(fd)){
+              // clear the buffer for next receiving.
+        int uartInputIndex = 0;
+        memset(uartInput, 0, UART_INPUT_MAX_SIZE+1);
+        // now receive data, till they are available
+        // Or whole string is received
+        while (serialDataAvail(conexion) > -1 &&
+                uartInputIndex < UART_INPUT_MAX_SIZE) {
+            uartInput[uartInputIndex] = serialGetchar(conexion);
+            if (0 == uartInput[uartInputIndex]) break; //zero terminator received
+            ++uartInputIndex;
+        }
+        if (0 == uartInputIndex) break; //No more strings received
+
+      std::cout << "Test"<< endl;
+        printf("%s\n", uartInput); // some string received
+      std::cout << "Test"<< endl;
+   }
+
+   /*while (serialDataAvail(fd))
    {
       char tmp = serialGetchar(fd);
       result[index] = tmp;
@@ -119,7 +141,7 @@ int SensorInterface::readSerial()
       std::cout << "EXCEPTION SERIAL" << std::endl;
       std::cout << tmpResult << std::endl;
    }
-
+*/
    serialClose(fd);
    return 0;
 }
