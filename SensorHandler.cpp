c8d372186ab5574da79335b6d8bc3b7ee41370d3
root <root@DESKTOP-UAFRR4C.localdomain>
2021-07-20 22:46:24 +0200

    fix

--- SensorHandler.cpp
+++ SensorHandler.cpp
@@ -5,133 +5,113 @@
 #include <cstring>
 #include <cstdint>
 #include <chrono>
+#include <thread>
+#include <thread> // std::thread
+#include <mutex>  // std::mutex
 #include <fstream>
 #include <sstream> //std::stringstream
-#include<ctime>
+#include <ctime>
+#include <thread>
 
+SensorHandler::SensorHandler()
+{
+    this->running = true;
 
+    this->configData = new ConfigData();
+    this->readConfig();
 
-
-
-
-
-SensorHandler::SensorHandler(){
-    
-    this->running = true; 
-    this->SensorReaderWriter();
+    sensorInterface = new SensorInterface();
 }
 
-SensorHandler::~SensorHandler(){
-
+SensorHandler::~SensorHandler()
+{
+    delete this->configData;
+    delete this->sensorInterface;
 }
 
-
-
-
-void SensorHandler::SensorReaderWriter(){
-
-   
-    
-
-    while(this->running){
-
-        SensorInterface* s1 = new SensorInterface(); 
-          
-        
-        std::cout<< s1->getSoilHumidity() << "  " << s1->getAirTemperature() << "  " << s1->getAirHumidity() << std::endl;
-        s1->setRelais(true);
+void sensorReaderWriter(SensorHandler *sensorHandler)
+{
+    std::cout << "starting Thread SensorReaderWriter" << std::endl;
+    while (sensorHandler->running)
+    {
+        std::cout << sensorHandler->sensorInterface->getSoilHumidity() << "  " << sensorHandler->sensorInterface->getAirTemperature() << "  " << sensorHandler->sensorInterface->getAirHumidity() << std::endl;
+        sensorHandler->sensorInterface->setRelais(true);
         //s1->writeJson();
 
-       
         std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-        
 
-        
-        s1->updateValues();
+        sensorHandler->sensorInterface->updateValues();
         //s1->writeJson();
 
+        std::cout << sensorHandler->sensorInterface->getSoilHumidity() << "  " << sensorHandler->sensorInterface->getAirTemperature() << "  " << sensorHandler->sensorInterface->getAirHumidity() << std::endl;
+        sensorHandler->sensorInterface->setRelais(false);
 
-        std::cout<< s1->getSoilHumidity() << "  " << s1->getAirTemperature() << "  " << s1->getAirHumidity() << std::endl;
-        s1->setRelais(false);
-         
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000)); 
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     }
-
-
-
 }
 
-
-
-
-void SensorHandler::WateringTimer(){
-
-    while(this->running){
-
-
-
-
+void wateringTimer(SensorHandler *sensorHandler)
+{
+    std::cout << "starting Thread WateringTimer" << std::endl;
+    while (sensorHandler->running)
+    {
     }
-
-
 }
 
-
-void SensorHandler::StopThreads(){
+void SensorHandler::StopThreads()
+{
     this->running = false;
 }
 
-
-
-
-void SensorHandler::readConfig(){
-    /*int mode= 0;
+void SensorHandler::readConfig()
+{
+    int mode = 0;
     int interval = 0;
-    int mode1duration =  0;
-    float threshold=  0;
-    float mode2duration =  0;
-    
-    try{
+    int mode1duration = 0;
+    float threshold = 0;
+    float mode2duration = 0;
 
-        ifstream inFile;
+    try
+    {
+
+        std::ifstream inFile;
         inFile.open("./src/config.json"); //open the input file
 
-        stringstream strStream;
-        strStream << inFile.rdbuf(); //read the file
-        string str = strStream.str(); //str holds the content of the file
+        std::stringstream strStream;
+        strStream << inFile.rdbuf();       //read the file
+        std::string str = strStream.str(); //str holds the content of the file
 
-        cout << str << "\n" << endl;; //you can do anything with the string!!!
+        std::cout << str << "\n"
+                  << std::endl;
+        ; //you can do anything with the string!!!
 
         size_t End = str.find("}");
         size_t modePos = str.find("mode") + 6;
         size_t modeEndPos = str.find(",");
         mode = stoi(str.substr(modePos, modeEndPos - modePos));
-        string rest = str.substr(modeEndPos+1, End - (modeEndPos+1));
+        std::string rest = str.substr(modeEndPos + 1, End - (modeEndPos + 1));
 
         size_t intervalPos = rest.find("interval") + 10;
         size_t intervalEndPos = rest.find(",");
         interval = stoi(rest.substr(intervalPos, intervalEndPos - intervalPos));
-        rest = rest.substr(intervalEndPos+1, End - (intervalEndPos+1));
+        rest = rest.substr(intervalEndPos + 1, End - (intervalEndPos + 1));
 
         size_t duration1Pos = rest.find("mode1duration") + 15;
         size_t duration1EndPos = rest.find(",");
         mode1duration = stoi(rest.substr(duration1Pos, duration1EndPos - duration1Pos));
-        rest = rest.substr(duration1EndPos+1, End - (duration1EndPos+1));
+        rest = rest.substr(duration1EndPos + 1, End - (duration1EndPos + 1));
 
-        
         size_t thresholdPos = rest.find("threshold") + 11;
         size_t thresholdEndPos = rest.find(",");
         threshold = stof(rest.substr(thresholdPos, thresholdEndPos - thresholdPos));
-        rest = rest.substr(thresholdEndPos+1, End - (thresholdEndPos+1));
+        rest = rest.substr(thresholdEndPos + 1, End - (thresholdEndPos + 1));
 
-        
         size_t duration2Pos = rest.find("mode2duration") + 15;
         size_t duration2EndPos = rest.find(",");
         mode2duration = stoi(rest.substr(duration2Pos, duration2EndPos - duration2Pos));
-        rest = rest.substr(duration2EndPos+1, End - (duration2EndPos+1));
+        rest = rest.substr(duration2EndPos + 1, End - (duration2EndPos + 1));
 
-        cout << " -->  Readed Config : "<<mode << " "<< interval << " "<<mode1duration << " "<<threshold << " "<<mode2duration << " " << endl;
+        std::cout << " -->  Readed Config : " << mode << " " << interval << " " << mode1duration << " " << threshold << " " << mode2duration << " " << std::endl;
 
         mtxConfigData.lock();
         this->configData->mode = mode;
@@ -140,19 +120,31 @@ void SensorHandler::readConfig(){
         this->configData->threshold = threshold;
         this->configData->mode2duration = mode2duration;
         mtxConfigData.unlock();
-
     }
-    catch (const std::exception& e) { 
+    catch (const std::exception &e)
+    {
 
-        cout << "Exception in REad Config" << endl;
+        std::cout << "Exception in REad Config" << std::endl;
         this->configData->mode = this->configData->mode;
         this->configData->interval = this->configData->interval;
         this->configData->mode1duration = this->configData->mode1duration;
         this->configData->threshold = this->configData->threshold;
         this->configData->mode2duration = this->configData->mode2duration;
-    }*/
+    }
 }
 
+int main(void)
+{
+    SensorHandler *sensorHandler = new SensorHandler();
 
+    std::thread t[2];
+    //Launch the sender/receiver thread
+    t[0] = std::thread(sensorReaderWriter, sensorHandler);
+    t[1] = std::thread(wateringTimer, sensorHandler);
 
+    //Join the threads with the main thread
+    t[0].join();
+    t[1].join();
 
+    return 0;
+}
--- SensorInterface.cpp
+++ SensorInterface.cpp
@@ -2,46 +2,43 @@
 #include <iostream>
 #include <stdexcept>
 #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 #include <sstream>
 
+#define PUMP 1
 
-
-#define PUMP     1
-
-
-using namespace std;
-
-
-SensorInterface::SensorInterface(){
+SensorInterface::SensorInterface()
+{
    soilHumidity = 0;
-   airTemperature= 0;
+   airTemperature = 0;
    airHumidity = 0;
    this->setDHTDate();
-   //this->readSerial();
-
+   this->readSerial();
 }
 
-
-SensorInterface::~SensorInterface(){
-  
+SensorInterface::~SensorInterface()
+{
 }
 
-string SensorInterface::executeShell(string command){
+std::string SensorInterface::executeShell(std::string command)
+{
 
-    char buffer[128];
-   string result = "";
+   char buffer[128];
+   std::string result = "";
 
    // Open pipe to file
-   FILE* pipe = popen(command.c_str(), "r");
-   if (!pipe) {
+   FILE *pipe = popen(command.c_str(), "r");
+   if (!pipe)
+   {
       return "popen failed!";
    }
    // read till end of process:
-   while (!feof(pipe)) {
+   while (!feof(pipe))
+   {
 
       // use buffer to read and add to result
       if (fgets(buffer, 128, pipe) != NULL)
@@ -51,120 +48,126 @@ string SensorInterface::executeShell(string command){
    return result;
 }
 
-
-void SensorInterface::setDHTDate(){
-   string ls = this->executeShell("python3 SensorDHTData.py");
-
+void SensorInterface::setDHTDate()
+{
+   std::string ls = this->executeShell("python3 SensorDHTData.py");
 
    int temperature = 20;
    int hummidity = 50;
-  
-   try {
-      temperature = stoi(ls.substr(ls.find("Temperature:")+12, ls.find("C") - (ls.find("Temperature:")+12)));
-      hummidity = stoi(ls.substr (ls.find("Humidity:")+9, ls.find("%") - (ls.find("Humidity:")+9)));
+
+   try
+   {
+      temperature = std::stoi(ls.substr(ls.find("Temperature:") + 12, ls.find("C") - (ls.find("Temperature:") + 12)));
+      hummidity = std::stoi(ls.substr(ls.find("Humidity:") + 9, ls.find("%") - (ls.find("Humidity:") + 9)));
 
       this->airTemperature = temperature;
       this->airHumidity = hummidity;
-
    }
-   catch (const std::exception& e) { 
-      if(this->airTemperature == 0 && this->airHumidity == 0){
+   catch (const std::exception &e)
+   {
+      if (this->airTemperature == 0 && this->airHumidity == 0)
+      {
          this->airTemperature = temperature;
          this->airHumidity = hummidity;
       }
-      else{
+      else
+      {
          this->airTemperature = this->airTemperature;
-         this->airHumidity =  this->airHumidity;
+         this->airHumidity = this->airHumidity;
       }
    }
 }
 
-
-/*int SensorInterface::readSerial(){
+int SensorInterface::readSerial()
+{
 
    int fd;
    // Setup serial port on ODROID
-   if ((fd = serialOpen ("/dev/ttyACM0",9600)) < 0) {
-      fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-      return 1 ;
+   if ((fd = serialOpen("/dev/ttyACM0", 9600)) < 0)
+   {
+      fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+      return 1;
    }
-   if (wiringPiSetup () == -1) {
-      fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-      return 1 ;
+   if (wiringPiSetup() == -1)
+   {
+      fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+      return 1;
    }
 
-   serialPrintf(fd,"A"); // send enter key to read data from sensor
+   serialPrintf(fd, "A"); // send enter key to read data from sensor
    delay(1000);
 
    char result[4];
-   int index =0;
+   int index = 0;
 
-   while (serialDataAvail (fd)) {
+   while (serialDataAvail(fd))
+   {
       char tmp = serialGetchar(fd);
       result[index] = tmp;
       index++;
-      cout << tmp << endl;
+      std::cout << tmp << std::endl;
    }
 
-   string tmpResult(result);  
-   try{ 
-      
-      this->soilHumidity = stof(tmpResult) /10;
+   std::string tmpResult(result);
+   try
+   {
 
+      this->soilHumidity = stof(tmpResult) / 10;
    }
-   catch (const std::exception& e) { 
-      cout << "EXCEPTION SERIAL" << endl;
-      cout << tmpResult << endl;
+   catch (const std::exception &e)
+   {
+      std::cout << "EXCEPTION SERIAL" << std::endl;
+      std::cout << tmpResult << std::endl;
    }
 
-
-   serialClose(fd); 
-   return 0; 
-}*/
-
-
-
-void SensorInterface::setRelais(bool on){
-     
-   #define PUMP     1
-   wiringPiSetup () ;
-   pinMode (1, OUTPUT) ;
-
-   if(on){
-      digitalWrite (1, 1) ;     // On
-
-   }
-   else{
-      digitalWrite (1, 0) ;     // Off
-   }  
+   serialClose(fd);
+   return 0;
 }
 
+void SensorInterface::setRelais(bool on)
+{
+   wiringPiSetup();
+   pinMode(1, OUTPUT);
 
-
-float SensorInterface::getSoilHumidity(){
+   if (on)
+   {
+      digitalWrite(1, HIGH); // On
+      delay (500) ;
+   }
+   else
+   {
+      digitalWrite(1, LOW); // Off
+      delay (500);
+   }
+}
+float SensorInterface::getSoilHumidity()
+{
    return this->soilHumidity;
 }
-int SensorInterface::getAirTemperature(){
+int SensorInterface::getAirTemperature()
+{
    return this->airTemperature;
 }
-int SensorInterface::getAirHumidity(){
+int SensorInterface::getAirHumidity()
+{
    return this->airHumidity;
 }
 
-void SensorInterface::updateValues(){
+void SensorInterface::updateValues()
+{
    this->setDHTDate();
-   //this->readSerial();
+   this->readSerial();
 }
 
-
-/*void SensorInterface::writeJson(){
+void SensorInterface::writeJson()
+{
    this->executeShell("sudo rm ./src/data.json");
-   string command = " echo \'{\n\t\"soilMoisture\": ";
-   command.append(to_string(this->soilHumidity));
+   std::string command = " echo \'{\n\t\"soilMoisture\": ";
+   command.append(std::to_string(this->soilHumidity));
    command.append(",\n\t\"temperature\":");
-   command.append(to_string(this->airTemperature));
+   command.append(std::to_string(this->airTemperature));
    command.append(",\n\t\"airHumidity\":");
-   command.append(to_string(this->airHumidity));
+   command.append(std::to_string(this->airHumidity));
    command.append("\n}\' > ./src/data.json");
    this->executeShell(command);
-}*/
+}
