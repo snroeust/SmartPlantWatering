38ac2cdb1c486a74cde93b051436441297e0c240
root <root@DESKTOP-UAFRR4C.localdomain>
2021-07-21 17:33:35 +0200

    fix

--- serialCommunication.py
+++ serialCommunication.py
@@ -38,7 +38,7 @@ while 1:
     #Read Data
     try:
         
-        soilMoisture = list(ser.readline().decode('utf-8'))[:-2]
+        soilMoisture = int(ser.readline().decode('utf-8'))
         result = instance.read()
 
         dataJson = {
