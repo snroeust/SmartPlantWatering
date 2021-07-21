f89cda53d3809c167fb5343bfdc46854c1a75022
root <root@DESKTOP-UAFRR4C.localdomain>
2021-07-21 17:09:53 +0200

    fix

--- serialCommunication.py
+++ serialCommunication.py
@@ -1,7 +1,23 @@
 import serial
-import time    
+import time 
+import json  
+import RPi.GPIO as GPIO
+import dht11
 
 global ser
+global instance
+
+try:
+    # initialize GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
+    # read data using pin 14
+    instance = dht11.DHT11(pin = 16)
+except:
+    print("GPIO Exception")
+
+
 try:
     #ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -10,10 +26,32 @@ except:
 
 ser.flush()
 
+
+dataJson = {
+    'soilMoisture': 0,
+    'temperature': 0,
+    'airHumidity': 0
+    }
+
 while 1:
-    print(ser.readline().decode('utf-8'))
-    time.sleep(1)
+   
+    #Read Data
+    try:
+        soilMoisture = ser.readline().decode('utf-8'))
+        result = instance.read()
+        dataJson = {'soilMoisture'} = soilMoisture
+        dataJson = {'temperature'} = result.temperature
+        dataJson = {'airHumidity'} = result.humidity
 
+        
+    except:
+        print("Serial communication Exception")
 
-"""inputValue = "Test"
-ser.write(inputValue.encode('utf-8'))"""
+    #write json
+    try:
+        with open('data.json', 'w') as outfile:
+            json.dump(dataJson, outfile)
+    except:
+        print("Write Json Exception")
+
+    time.sleep(10)
